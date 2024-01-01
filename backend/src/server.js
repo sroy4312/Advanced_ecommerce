114beecea1480a95f3e19c02851e77a0bbaa6424
sroy4312 <145863894+sroy4312@users.noreply.github.com>
2024-01-01 21:55:28 +0530

    Delete backend directory

--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./index");
-const connectDb = require("./database/db");
-
-
-app.listen(process.env.SERVER_PORT, async() => {
-    await connectDb();
-    console.log(`Server is running on port ${process.env.SERVER_PORT}`);
-})
